9b6f2be12cebf892a24d1560e0b70572aadf3514
ananya92 <1992ananya@gmail.com>
2020-03-16 17:26:44 +1030

    Added post api to create new workout and get api to return all workouts for stats

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,34 @@
 var Workout = require("../models/workoutModel");
+var mongoose = require('mongoose');
 
 module.exports = function(app) {
     app.get("/api/workouts", function(req, res) {
     	Workout.find({}).then(function(dbWorkouts) {
       		res.json(dbWorkouts);
-    	})
-    });
+		})
+		.catch(err => {
+			res.json(err);
+		});
+	});
+
+	//Create a new workout. Generating a new workout ID
+	app.post("/api/workouts", function(req, res) {
+		Workout.create(req.body).then(function(dbWorkout) {
+			console.log('Workout added', dbWorkout);
+			res.json(dbWorkout);
+		})
+		.catch(err => {
+			res.json(err);
+		});
+	});
+
+	//Get all the workouts from database for preparing stats
+	app.get("/api/workouts/range", function(req, res) {
+    	Workout.find({}).then(function(dbWorkouts) {
+			res.json(dbWorkouts);
+	  })
+	  .catch(err => {
+		  res.json(err);
+	  });
+	});
 }
