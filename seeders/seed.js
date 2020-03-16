8eff89d8269bd01eb8101473e6a4b09999498ca9
ananya92 <1992ananya@gmail.com>
2020-03-16 11:58:01 +1030

    Added missing totalDuration to the seed data

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 let mongoose = require("mongoose");
-let db = require("../models");
+let Workout = require("../models/workoutModel");
 
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
@@ -9,6 +9,7 @@ mongoose.connect("mongodb://localhost/workout", {
 let workoutSeed = [
   {
     day: new Date().setDate(new Date().getDate()-10),
+    totalDuration: 20,
     exercises: [
       {
         type: "resistance",
@@ -22,6 +23,7 @@ let workoutSeed = [
   },
   {
     day: new Date().setDate(new Date().getDate()-9),
+    totalDuration: 20,
     exercises: [
       {
         type: "resistance",
@@ -35,6 +37,7 @@ let workoutSeed = [
   },
   {
     day: new Date().setDate(new Date().getDate()-8),
+    totalDuration: 25,
     exercises: [
       {
         type: "resistance",
@@ -48,6 +51,7 @@ let workoutSeed = [
   },
   {
     day: new Date().setDate(new Date().getDate()-7),
+    totalDuration: 25,
     exercises: [
       {
         type: "cardio",
@@ -59,6 +63,7 @@ let workoutSeed = [
   },
   {
     day: new Date().setDate(new Date().getDate()-6),
+    totalDuration: 20,
     exercises: [
       {
         type: "resistance",
@@ -72,6 +77,7 @@ let workoutSeed = [
   },
   {
     day: new Date().setDate(new Date().getDate()-5),
+    totalDuration: 20,
     exercises: [
       {
         type: "resistance",
@@ -85,6 +91,7 @@ let workoutSeed = [
   },
   {
     day: new Date().setDate(new Date().getDate()-4),
+    totalDuration: 30,
     exercises: [
       {
         type: "resistance",
@@ -98,6 +105,7 @@ let workoutSeed = [
   },
   {
     day: new Date().setDate(new Date().getDate()-3),
+    totalDuration: 20,
     exercises: [
       {
         type: "resistance",
@@ -111,6 +119,7 @@ let workoutSeed = [
   },
   {
     day: new Date().setDate(new Date().getDate()-2),
+    totalDuration: 20,
     exercises: [
       {
         type: "resistance",
@@ -124,9 +133,10 @@ let workoutSeed = [
   },
   {
     day: new Date().setDate(new Date().getDate()-1),
+    totalDuration: 30,
     exercises: [
       {
-        type: "resistance",
+        type: "cardio",
         name: "Bench",
         duration: 30,
         distance: 2
@@ -135,8 +145,8 @@ let workoutSeed = [
   }
 ];
 
-db.Workout.deleteMany({})
-  .then(() => db.Workout.collection.insertMany(workoutSeed))
+Workout.deleteMany({})
+  .then(() => Workout.collection.insertMany(workoutSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
